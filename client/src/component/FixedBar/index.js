9309cf9ad64a0f5eb3582521700648004308183b
云思 <james.hsy@alibaba-inc.com>
2021-04-05 12:45:22 +0800

    fix: fix fixedbar

--- client/src/component/FixedBar/index.js
+++ client/src/component/FixedBar/index.js
@@ -1,35 +1,33 @@
-import React from 'react'
-import './style.less'
-import { Icon as SemiIcon } from 'semantic-ui-react'
-import { Icon } from 'antd'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import './style.less';
+import { EditOutlined, ArrowUpOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
 
 const scrollToTop = () => {
-	const c = document.documentElement.scrollTop || document.body.scrollTop
-	if (c > 0) {
-		window.requestAnimationFrame(scrollToTop)
-		window.scrollTo(0, c - c / 8)
-	}
-}
+  const c = document.documentElement.scrollTop || document.body.scrollTop;
+  if (c > 0) {
+    window.requestAnimationFrame(scrollToTop);
+    window.scrollTo(0, c - c / 8);
+  }
+};
 
-export default () => (
-	<div className="g-fixedbar">
-		<Link to='/write'>
-			<div
-				className="m-icon m-red"
-				onClick={() => {
-				}}>
-				<Icon type="edit" />
-			</div>
-		</Link>
+const FixedBar = () => (
+  <div className='g-fixedbar'>
+    <Link to='/write'>
+      <div className='m-icon m-red' onClick={() => {}}>
+        <EditOutlined />
+      </div>
+    </Link>
 
-		<div
-			className="m-icon m-grey"
-			onClick={() => {
-				scrollToTop()
-			}}
-		>
-			<SemiIcon name="arrow up" />
-		</div>
-	</div>
-)
+    <div
+      className='m-icon m-grey'
+      onClick={() => {
+        scrollToTop();
+      }}
+    >
+      <ArrowUpOutlined />
+    </div>
+  </div>
+);
+
+export default FixedBar;
